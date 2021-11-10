450181e0991f609235884577d885d80286624664
TexHexx <prior_it@mail.ru>
2021-11-10 20:28:57 +0300

    Add project files.

---
+++ HomeWork_HM_7/HomeWork_HM_7.cpp
@@ -0,0 +1,12 @@
+﻿// HomeWork_HM_7.cpp : Defines the entry point for the application.
+//
+
+#include "HomeWork_HM_7.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
